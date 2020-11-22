cf0f5ccb32d9475e72608264eed34870affaa4f2
gudwieser <goleandr@gmail.com>
2020-11-22 20:04:27 +0100

    add window.onload

--- main/main.js
+++ main/main.js
@@ -1,14 +1,24 @@
+{
+        let input1 = document.getElementById("firstValue");
+        let input2 = document.getElementById("secondValue");
 
-let input1 = document.getElementById("firstValue");
-let input2 = document.getElementById("secondValue");
+        let num1 = parseFloat(input1.value);
+        let num2 = parseFloat(input2.value);
 
-let num1 = parseFloat(input1.value);
-let num2 = parseFloat(input2.value);
 
-let action = document.getElementById("btn")
 
-action.addEventListener('click', function () {
-        console.log(num1 + num2);
-});
+
+        window.onload = function (){
+                function sum(num1, num2) {
+                        console.log(num1+num2);
+                }
+
+                let action = document.getElementById("btn")
+        action.addEventListener('click',  function () {
+                console.log("Wartość sumy: " + num1 + num2);
+        });
+
+        }
+}
 
 
