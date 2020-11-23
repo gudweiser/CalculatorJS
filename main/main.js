5d134b86e7e9b4abb8047f0076db3269aa5b8b56
gudwieser <goleandr@gmail.com>
2020-11-23 21:37:10 +0100

    add answer in html

--- main/main.js
+++ main/main.js
@@ -1,24 +1,29 @@
-{
-        let input1 = document.getElementById("firstValue");
-        let input2 = document.getElementById("secondValue");
 
-        let num1 = parseFloat(input1.value);
-        let num2 = parseFloat(input2.value);
+window.onload = function (){
 
+        function sum() {
 
+                let input1 = document.getElementById("firstValue");
+                let input2 = document.getElementById("secondValue");
 
+                let num1 = parseFloat(input1.value);
+                let num2 = parseFloat(input2.value);
+                let nums = num1 + num2;
 
-        window.onload = function (){
-                function sum(num1, num2) {
-                        console.log(num1+num2);
-                }
+                console.log("Wartość sumy: " + nums, typeof num1);
 
-                let action = document.getElementById("btn")
-        action.addEventListener('click',  function () {
-                console.log("Wartość sumy: " + num1 + num2);
-        });
-
+                document.getElementById("answer").innerHTML = nums;
         }
+
+        let action = document.getElementById("btn")
+        action.addEventListener('click',  sum);
+
+
+
+
+
+
 }
+
 
 
