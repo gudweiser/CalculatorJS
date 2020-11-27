3ad9c6781189e78cc49492ad3df223f5fb95f249
gudwieser <goleandr@gmail.com>
2020-11-27 21:23:58 +0100

    add options

--- main/main.js
+++ main/main.js
@@ -1,58 +1,37 @@
 window.onload = function () {
 
-    function sum() {
-        let input1 = document.getElementById("firstValue");
-        let input2 = document.getElementById("secondValue");
-        let select = document.getElementById("select");
-        let action = select.options[select.selectedIndex].text;
-        console.log("Action: " + action);
-        let num1 = parseFloat(input1.value);
-        let num2 = parseFloat(input2.value);
+    function count() {
 
-        switch (action) {
-            case 'Addition':
-                sum(num1, num2);
+        let num1 = parseFloat(document.getElementById("firstValue").value);
+        let num2 = parseFloat(document.getElementById("secondValue").value);
+        let select = document.getElementById("select").value;
+        let result = document.getElementById("answer");
+
+        console.log("Action: " + select);
+
+        switch (select) {
+            case 'sum':
+                result.innerText = (num1 + num2);
                 break;
-            case 'Subtraction':
-                subtraction(num1, num2);
+            case 'sub':
+                result.innerText = (num1 - num2);
                 break;
-            case 'Multiplcation' :
-                multiplication(num1, num2);
+            case 'multi' :
+                result.innerText = (num1 * num2);
                 break;
-            case 'Division':
-                division(num1, num2);
+            case 'div':
+                result.innerText = (num1 / num2);
                 break;
+            default:
+                console.log("Sorry, wrong option")
 
         }
 
-
-        function sum(num1, num2) {
-            const result = num1 + num2
-            return result;
-        }
-
-        function subtraction(num1, num2) {
-            let result = num1 - num2;
-            return num1 - num2;
-        }
-
-        function multiplication(num1, num2) {
-            let result = num1 * num2;
-            return result;
-        }
-
-        function division(num1, num2) {
-            let result = num1 / num2;
-            return result;
-        }
-
-        console.log("Wartość sumy: " + result, typeof num1);
-
-        document.getElementById("answer").innerHTML = result;
     }
 
     let action = document.getElementById("btn")
-    action.addEventListener('click', sum);
+    action.addEventListener('click', count);
+
 
 
 }
